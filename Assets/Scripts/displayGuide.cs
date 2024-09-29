e208676dff767cb87de0f82f10cc65e3a3134e32
vw79 <wshong8282@gmail.com>
2024-09-29 14:03:05 +0800

    basic win

--- Assets/Scripts/displayGuide.cs
+++ Assets/Scripts/displayGuide.cs
@@ -7,31 +7,35 @@ public class displayGuide : MonoBehaviour
     public GameObject guide;
     public GrabObejct grabObject;
 
+    private string objectTag;
+
     void Start()
     {
         guide.SetActive(false);
-    }
-
-    private void Update()
-    {
-        if (!grabObject.isHolding)
-        {
-            guide.SetActive(false);
-        }
+        objectTag = gameObject.tag;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
+        // Ensures that the guide is shown only if player is in range and conditions are met
         if (collision.gameObject.tag == "Player")
         {
-            if (grabObject.isHolding)
+            if (objectTag == "Generator" && grabObject.isHolding)
             {
                 guide.SetActive(true);
+            }
+            else if (objectTag == "Door" && grabObject.isDoorUnlocked)
+            {
+                guide.SetActive(true);
+            }
+            else
+            {
+                guide.SetActive(false);
             }
         }
     }
 
-        private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
